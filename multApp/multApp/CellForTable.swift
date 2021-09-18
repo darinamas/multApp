23908154d9c80980d3a27d3f367026d1711f9c64
Darina <daryna.massive@gmail.com>
2021-09-18 14:23:51 +0200

    Added tableview and cells

---
+++ multApp/multApp/CellForTable.swift
@@ -0,0 +1,13 @@
+//
+//  CellForTable.swift
+//  multApp
+//
+//  Created by Daryna Polevyk on 18.09.2021.
+//
+
+import UIKit
+
+class CellForTable: UITableViewCell {
+
+
+}
--- multApp/multApp/ViewController.swift
+++ multApp/multApp/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CellForTable
+        cell.backgroundColor = .blue
+        return cell
+    }
+    
+    
+    
+}
