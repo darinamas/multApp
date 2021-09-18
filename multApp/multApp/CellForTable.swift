53a578d0d96db37e21ed486763642af1fa038d82
Darina <daryna.massive@gmail.com>
2021-09-18 15:24:20 +0200

    added cell view added dictionary
    header for section

--- multApp/multApp/CellForTable.swift
+++ multApp/multApp/CellForTable.swift
@@ -8,6 +8,23 @@
 import UIKit
 
 class CellForTable: UITableViewCell {
+    var label: UILabel = {
+        let label = UILabel(frame: CGRect(x: 20, y: 20, width: 200, height: 50))
+        label.textColor = .green
+        return label
+    }()
 
-
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        addSubview()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+   private func addSubview() {
+        addSubview(label)
+        
+    }
 }
--- multApp/multApp/ViewController.swift
+++ multApp/multApp/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    var data2 = ["2" : ["2^1 = 2", "2^2 = 4", "2^3 = 8"]]
+    var data3 = ["3" : ["3^1 = 3", "3^2 = 9", "3^3 = 27"], "2" : ["2^1 = 2", "2^2 = 4", "2^3 = 8"]]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.register(CellForTable.self, forCellReuseIdentifier: "cell")
+        
+        let a = data3["3"]
+        print(a![0])
     }
 
 
@@ -27,16 +33,40 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return section == 0 ? 1 : 3
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
     let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CellForTable
         cell.backgroundColor = .blue
+      //  cell.label.text = "Section: \(indexPath.section) Row: \(indexPath.row)"
+        
+        if indexPath.section == 0 {
+            cell.label.text = "Hello 0"
+        } else {
+            let a = data3["3"]
+            print(a![indexPath.row])
+            cell.label.text = a![indexPath.row]
+        }
+        
         return cell
     }
     
-    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        let view = UIView()
+        let label = UILabel()
+        label.text = section == 0 ?  " " : "Cтепени"
+        label.backgroundColor = UIColor.lightGray
+        label.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 30)
+        view.addSubview(label)
+        
+        return view
+
+    }
     
 }
+
+
+
